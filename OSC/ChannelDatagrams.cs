2a01d4fe552063fb95b8836593a4cd40ae703082
Ophura <8chan.com@gmail.com>
2025-05-04 19:15:27 +0200

    Update ChannelDatagrams.cs

--- OSC/ChannelDatagrams.cs
+++ OSC/ChannelDatagrams.cs
@@ -1,28 +1,31 @@
 using System;
 
-namespace Ophura;
+namespace Projection;
 
 internal readonly ref struct ChannelDatagrams
 {
-    private readonly Span<byte> S0;
-    private readonly Span<byte> S1;
-    private readonly Span<byte> S2;
+    private readonly Span<byte> RDatagram;
+    private readonly Span<byte> GDatagram;
+    private readonly Span<byte> BDatagram;
 
-    internal int Count { get; init; }
+    internal static int Count => 3;
 
-    internal ChannelDatagrams(Span<byte> S0, Span<byte> S1, Span<byte> S2)
+    internal ChannelDatagrams(Span<byte> RDatagram, Span<byte> GDatagram, Span<byte> BDatagram)
     {
-        this.S0 = S0;
-        this.S1 = S1;
-        this.S2 = S2;
-        Count = 3;
+        "/avatar/parameters/R\0\0\0\0,f\0\0\0\0\0\0"u8.CopyTo(RDatagram);
+        "/avatar/parameters/G\0\0\0\0,f\0\0\0\0\0\0"u8.CopyTo(GDatagram);
+        "/avatar/parameters/B\0\0\0\0,f\0\0\0\0\0\0"u8.CopyTo(BDatagram);
+
+        this.RDatagram = RDatagram;
+        this.GDatagram = GDatagram;
+        this.BDatagram = BDatagram;
     }
 
     internal Span<byte> this[int Index] => Index switch
     {
-        0 => S0,
-        1 => S1,
-        2 => S2,
+        0 => RDatagram,
+        1 => GDatagram,
+        2 => BDatagram,
         _ => throw new ArgumentOutOfRangeException(nameof(Index))
     };
 }
